75da3df11cbb8831e5eaa0304a4e05bf7c6dafe3
cbj0353 <cbj0353@naver.com>
2021-07-20 21:44:46 +0900

    7/20(화) 문제 풀이
    프로그래머스 레벨2.주식가격

---
+++ Programmers_Lv2/210720_Programmers_lv2_주식가격.py
@@ -0,0 +1,15 @@
+#레벨2. 주식가격
+#https://programmers.co.kr/learn/courses/30/lessons/42584?language=python3
+
+def solution(prices):
+    answer = []
+    for i in range(len(prices)-1):
+        cnt=0
+        for j in range(i+1,len(prices)) : 
+            cnt+=1
+            if prices[i]>prices[j] : 
+                break   
+        answer.append(cnt)
+    answer.append(0)
+    
+    return answer
